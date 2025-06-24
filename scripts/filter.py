0515ab8ef90343006e322824d725e321237335c5
gonzalo-gigena <gigena.gonzalo@outlook.com>
2025-06-24 10:32:03 -0300

    New scripts folder

--- scripts/filter.py
+++ scripts/filter.py
@@ -37,7 +37,7 @@ def is_image_empty(image_path, threshold=0.95, darkness_threshold=10):
     print(f"Error processing {image_path}: {str(e)}")
     return False
 
-def delete_empty_images(folder_path, threshold, delete):
+def delete_empty_images(folder_path, threshold, delete, frames):
   """
   Check all images in a folder and delete empty (mostly black) images
   
@@ -46,57 +46,30 @@ def delete_empty_images(folder_path, threshold, delete):
     threshold (float): Threshold for determining if image is empty
   """
   # Supported image extensions
-  valid_extensions = ('.png', '.jpg', '.jpeg', '.tiff', '.bmp')
+  valid_extensions = ('.jpg')
   
   # Check if folder exists
   if not os.path.exists(folder_path):
     print(f"Error: Folder {folder_path} does not exist!")
     return
   
+  files=  [f for f in os.listdir(folder_path) if f.startswith('cubesat')]
+  files.sort() # The order of files is importat
+  
   # Iterate through all files in the folder
-  for filename in os.listdir(folder_path):
-    if filename.lower().endswith(valid_extensions):
-      file_path = os.path.join(folder_path, filename)
-      
-      if is_image_empty(file_path, threshold):
+  for i in range(0, len(files), frames):
+    are_empty = True
+    images = []
+    for j in range(frames):
+      if files[i+j].lower().endswith(valid_extensions):
+        file_path = os.path.join(folder_path, files[i+j])
+        are_empty = are_empty and is_image_empty(file_path, threshold)
+        images.append(file_path)
+        
+    if delete and are_empty:
+      for j in range(frames):
         try:
           if delete:
-            os.remove(file_path)
+            os.remove(images[j])
         except Exception as e:
-          print(f"Error deleting {filename}: {str(e)}")
-
-def parse_arguments():
-  """
-  Parse command line arguments
-  
-  Returns:
-    argparse.Namespace: Parsed command line arguments
-  """
-  parser = argparse.ArgumentParser(description='Delete empty (mostly black) images from a folder')
-  
-  parser.add_argument('--path', 
-                    type=str,
-                    required=True,
-                    help='Path to the folder containing images')
-  
-  parser.add_argument('--threshold',
-                    type=float,
-                    default=0.95,
-                    help='Threshold for determining if an image is empty (0.0 to 1.0)')
-  
-  parser.add_argument('--delete',
-                    action='store_true',
-                    help='Delete images below the threshold')
-  
-  args = parser.parse_args()
-  
-  # Validate threshold
-  if not 0 <= args.threshold <= 1:
-    parser.error("Threshold must be between 0.0 and 1.0")
-  
-  return args
-
-if __name__ == "__main__":
-  args = parse_arguments()
-  
-  delete_empty_images(args.path, args.threshold, args.delete)
+          print(f"Error deleting {images[j]}: {str(e)}")
--- src/preprocessing/__main__.py
+++
@@ -1,9 +0,0 @@
-from .args import parse_arguments
-from .filter import delete_empty_images
-
-def main():
-  args = parse_arguments()
-  delete_empty_images(args.path, args.threshold, args.delete)
-
-if __name__ == "__main__":
-  main()
